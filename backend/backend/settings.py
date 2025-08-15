d4da57243d925b3e3b337a48c5484d26dd992dde
muhammadmasoud <mebramasoud@gmail.com>
2025-08-15 05:30:47 +0300

    a7aaaa

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -153,14 +153,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'portfolio.User'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
-    "http://localhost:8081",
-    "http://127.0.0.1:8081",
+    "http://localhost:80",
+    "http://127.0.0.1:80"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
